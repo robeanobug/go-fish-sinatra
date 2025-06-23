9ab7752a5b0c7329b04a8453b675bc2bfc45e785
robeanobug <fakeemail@gmail.com>
2025-06-23 10:31:37 -0400

    add capybara test

---
+++ spec/server_spec.rb
@@ -0,0 +1,20 @@
+require 'rack/test'
+require 'rspec'
+require 'capybara'
+require 'capybara/dsl'
+ENV['RACK_ENV'] = 'test'
+require_relative '../server'
+
+RSpec.describe Server do
+  include Capybara::DSL
+  before do
+    Capybara.app = Server.new
+  end
+  it 'is possible to join a game' do
+    visit '/'
+    fill_in :name, with: 'John'
+    click_on 'Join'
+    expect(page).to have_content('Players')
+    expect(page).to have_content('John')
+  end
+end
