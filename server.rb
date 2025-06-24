17e8e21565a499d64add9e701a48c6b9b511455d
robeanobug <fakeemail@gmail.com>
2025-06-24 08:47:19 -0400

    add server start up information

--- server.rb
+++ server.rb
@@ -9,4 +9,28 @@ class Server < Sinatra::Base
   enable :sessions
   register Sinatra::RespondWith
   use Rack::JSONBodyParser
+
+  def self.game
+    @@game ||= Game.new
+  end
+
+  get '/' do
+    slim :index
+  end
+
+  get '/lobby' do
+    slim :lobby, locals: { current_player: session[:current_player] }
+  end
+
+  post '/join' do
+    player = Player.new(params['name'])
+    session[:current_player] = player
+    self.class.game.add_player(player)
+    redirect '/lobby'
+  end
+
+  get '/game' do
+    redirect '/' if self.class.game.empty?
+    slim :game, locals: { game: self.class.game, current_player: session[:current_player] }
+  end
 end
