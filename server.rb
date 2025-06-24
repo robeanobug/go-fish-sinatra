fae9f927ef12f3850683d95539022a5607dc9cfb
robeanobug <fakeemail@gmail.com>
2025-06-24 13:57:31 -0400

    add api verification

--- server.rb
+++ server.rb
@@ -2,8 +2,10 @@ require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/respond_with'
 require 'rack/contrib'
+require 'securerandom'
 require_relative 'lib/game'
 require_relative 'lib/player'
+require_relative 'lib/user'
 
 class Server < Sinatra::Base
   enable :sessions
@@ -14,23 +16,64 @@ class Server < Sinatra::Base
     @@game ||= Game.new
   end
 
+  # def reset_state!
+  #   @@game = nil
+  # end
+
   get '/' do
     slim :index
   end
 
   get '/lobby' do
-    slim :lobby, locals: { current_player: session[:current_player] }
+    redirect :game unless self.class.game.empty?    
+    respond_to do |f|
+      f.json {
+        json api_key: session[:current_user].api_key
+      }
+      f.html { slim :lobby, locals: { game: self.class.game, current_player: session[:current_player] } }
+    end
   end
 
   post '/join' do
-    player = Player.new(params['name'])
-    session[:current_player] = player
-    self.class.game.add_player(player)
-    redirect '/lobby'
+    player = create_player
+    session[:current_user] = create_user(player)
+    respond_to do |f|
+      f.json {
+        json api_key: session[:current_user].api_key
+      }
+      f.html { redirect '/lobby' }
+    end
   end
 
   get '/game' do
-    redirect '/' if self.class.game.empty?
-    slim :game, locals: { game: self.class.game, current_player: session[:current_player] }
+    redirect '/' if self.class.game.nil?
+    respond_to do |f|
+      f.json {
+        json api_key: session[:current_user].api_key
+        json players: self.class.game.players
+      }
+      f.html { slim :game, locals: { game: self.class.game, current_user: session[:current_user] } }
+    end
+  end
+
+  get '/play' do
+    response_to do |f|
+      f.json {
+        json api_key: session[:current_user].api_key
+        json players: self.class.game.players
+      }
+      f.html { slim :game, locals: { game: self.class.game, current_user: session[:current_user] } }
+    end
+  end
+
+  def create_user(player)
+    api_key = SecureRandom.urlsafe_base64(24)
+    User.new(player, api_key)
+  end
+
+  def create_player
+    player = Player.new(params['name'])
+    self.class.game.add_player(player)
+    player
   end
 end
