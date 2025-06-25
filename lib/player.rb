d8757bf62d4d4e8787b13bdde4d353c453c9fb4c
robeanobug <fakeemail@gmail.com>
2025-06-25 08:40:55 -0400

    add add_cards method to player for player hand

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,13 @@
 class Player
   attr_reader :name
+  attr_accessor :hand
   def initialize(name)
     @name = name
+    @hand = []
+  end
+
+  def add_cards(cards)
+    return hand << cards if cards.is_a?(PlayingCard)
+    cards.each { |card| hand << card }
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,8 +3,17 @@ require "spec_helper"
 
 RSpec.describe Player do
   let(:player) { Player.new("Player")}
+  let(:ace_clubs) {PlayingCard.new('Ace', 'Clubs') }
+  let(:ace_hearts) {PlayingCard.new('Ace', 'Hearts') }
 
   it 'has a name' do
     expect(player.name).to eq("Player")
+  end
+  it 'has a hand' do
+    expect(player.hand).to be_a(Array)
+  end
+  it 'adds cards to the hand' do
+    player.add_cards([ace_clubs, ace_hearts])
+    expect(player.hand).to eq([ace_clubs, ace_hearts])
   end
 end
